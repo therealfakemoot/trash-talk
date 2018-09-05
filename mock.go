e385e67164f1d7761f56b03d3e7de0a3974c2392
Nick Dumas <nicholas.dumas@homes.com>
2018-09-05 15:02:30 +0000

    Initial commit.

---
+++ mock.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// Mock accepts a string and returns alternating case string.
+func Mock(s string) string {
+	b := strings.Builder{}
+	reg, _ := regexp.Compile("[^\\p{L}\\p{Z}]+")
+
+	n := reg.ReplaceAllString(s, "")
+	b.Grow(len(n))
+	for i, c := range n {
+		if i%2 == 0 {
+			b.WriteString(strings.ToUpper(string(c)))
+		} else {
+			b.WriteString(strings.ToLower(string(c)))
+		}
+	}
+	return b.String()
+}
+
+func main() {
+	var s string
+	flag.StringVar(&s, "in", "", "Any string can be passed in.")
+
+	flag.Parse()
+
+	// fmt.Println("Input string: `" + s + "`")
+
+	fmt.Println(Mock(s))
+
+}
